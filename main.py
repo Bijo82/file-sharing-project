0c66dde64933a4ed4112897da92aa2e541e3054d
Bijo82 <bijosalu810@gmail.com>
2026-06-22 00:01:23 +0530

    Changes in main.py

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import asyncio
 from services.cleanup_services import cleanup_task
 from routes.files import router as FileRouter
 from routes.texts import router as TextRouter
+import os
 
+os.makedirs("uploads", exist_ok=True)
+os.makedirs("texts", exist_ok=True)
 app = FastAPI()
 
 app.include_router(FileRouter)
