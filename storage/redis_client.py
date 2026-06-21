fcf49dfad531935cc5d90c138b612c92d32f782d
Bijo82 <bijosalu810@gmail.com>
2026-06-22 00:08:35 +0530

    More changes in redis file

--- storage/redis_client.py
+++ storage/redis_client.py
@@ -1,9 +1,16 @@
 import os
 import redis
 
+#Also allows local deployment
 Redis = redis.Redis(
-    host=os.getenv("REDIS_HOST"),
-    port=int(os.getenv("REDIS_PORT")),
+    host=os.getenv("REDIS_HOST", "localhost"),
+    port=int(os.getenv("REDIS_PORT", 6379)),
     password=os.getenv("REDIS_PASSWORD"),
     decode_responses=True
 )
+
+try:
+    Redis.ping()
+    print("Redis Connected")
+except Exception as e:
+    print("Redis Connection Failed:", e)
