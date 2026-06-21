9f1fd7aa672e164bf16d991a8625f3b544ce3770
Bijo82 <bijosalu810@gmail.com>
2026-06-22 00:30:19 +0530

    More changes in redis file

--- storage/redis_client.py
+++ storage/redis_client.py
@@ -6,7 +6,8 @@ Redis = redis.Redis(
     host=os.getenv("REDIS_HOST", "localhost"),
     port=int(os.getenv("REDIS_PORT", 6379)),
     password=os.getenv("REDIS_PASSWORD"),
-    decode_responses=True
+    decode_responses=True,
+    ssl=True
 )
 
 try:
