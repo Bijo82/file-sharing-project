bff378a5f737aa4ec062cb24c4652b98c5379868
Bijo82 <bijosalu810@gmail.com>
2026-06-23 12:25:03 +0530

    Otps changed to uppercase and numbers

--- services/otp_services.py
+++ services/otp_services.py
@@ -2,6 +2,6 @@ import secrets
 import string
 
 def otpgenerate(length=6):
-    characters = string.ascii_letters+string.digits
+    characters = string.ascii_uppercase+string.digits
     otp = "".join(secrets.choice(characters) for _ in range(length))
     return otp
